1f9ef4c9a2d89c3ad967d4eedeece08fdafd05e7
Mykola Maik <mykoluamaik@gmail.com>
2023-05-08 01:03:54 +0300

    MyBiKE landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// const openButton = document.querySelector('#menuOpenButton');
+// const closeButton = document.querySelector('#menuCloseButton');
+// const menu = document.querySelector('#menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// window.addEventListener('scroll', () => {
+//   const scrolled = window.scrollY;
+
+//   console.log(scrolled);
+// });
